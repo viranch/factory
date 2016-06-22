9a59cb9ad71b37ccce71cd71fd9a78bea3ae0a6b
Viranch Mehta <viranch.mehta@gmail.com>
2016-06-22 10:03:31 +0530

    Stop worker threads after jobs are finished

--- __init__.py
+++ __init__.py
@@ -11,6 +11,10 @@ class Factory:
     def worker(self):
         while True:
             item = self.q.get()
+            if item is None:
+                # stop worker once all is done
+                self.q.task_done()
+                break
             output = self.worker_fn(item)
             self.results.append(output)
             self.q.task_done()
@@ -25,9 +29,17 @@ class Factory:
         for x in jobs:
             self.q.put(x)
 
+    def stop_threads(self):
+        for x in range(self.workers):
+            self.q.put(None)
+        self.q.join()
+
     def work(self, jobs):
+        if None in jobs:
+            raise Exception("'NoneType' cannot be in jobs")
         self.results = []
         self.start_threads()
         self.put_jobs(jobs)
         self.q.join()
+        self.stop_threads()
         return self.results
